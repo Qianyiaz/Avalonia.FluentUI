7874de091a0ce635565c6c93545cde78fe3f02d3
Qusay Diaz <Qusay_Diaz.github@outlook.com>
2026-03-02 11:50:02 +0800

    优化性能

--- Avalonia.FluentUI/Views/Windows/MainWindow.axaml.cs
+++ Avalonia.FluentUI/Views/Windows/MainWindow.axaml.cs
@@ -5,37 +5,41 @@ using FluentAvalonia.UI.Controls;
 namespace Avalonia.FluentUI.Views.Windows;
 
 public partial class MainWindow : Window
-{
-    public MainWindow() => InitializeComponent();
+{ 
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
 
     protected override void OnOpened(EventArgs e)
     {
         base.OnOpened(e);
         RootNavigation.SelectedItem = RootNavigation.MenuItems[0];
     }
-
+    
     [DynamicDependency(DynamicallyAccessedMemberTypes.PublicConstructors, typeof(HomePage))]
     [DynamicDependency(DynamicallyAccessedMemberTypes.PublicConstructors, typeof(LoginPage))]
     [DynamicDependency(DynamicallyAccessedMemberTypes.PublicConstructors, typeof(ListBoxPage))]
     [DynamicDependency(DynamicallyAccessedMemberTypes.PublicConstructors, typeof(TextBoxPage))]
     [DynamicDependency(DynamicallyAccessedMemberTypes.PublicConstructors, typeof(SettingsPage))]
     [DynamicDependency(DynamicallyAccessedMemberTypes.PublicConstructors, typeof(RadioButtonPage))]
+    private static readonly Dictionary<string, Type> PageMap = new()
+    {
+        ["Home"] = typeof(HomePage),
+        ["Login"] = typeof(LoginPage),
+        ["ListBox"] = typeof(ListBoxPage),
+        ["TextBox"] = typeof(TextBoxPage),
+        ["Settings"] = typeof(SettingsPage),
+        ["RadioButton"] = typeof(RadioButtonPage)
+    };
+    
     private void OnSelectionChanged(object sender, NavigationViewSelectionChangedEventArgs e)
     {
         if (e.SelectedItem is not NavigationViewItem item) return;
 
-        Frame.Navigate
-        (
-            item.Content switch
-            {
-                "Home" => typeof(HomePage),
-                "Login" => typeof(LoginPage),
-                "ListBox" => typeof(ListBoxPage),
-                "TextBox" => typeof(TextBoxPage),
-                "Settings" => typeof(SettingsPage),
-                "RadioButton" => typeof(RadioButtonPage),
-                _ => throw new NotImplementedException()
-            }
-        );
+        if (item.Content is string key && PageMap.TryGetValue(key, out var type))
+            Frame.Navigate(type);
+        else
+            throw new NotImplementedException();
     }
 }
